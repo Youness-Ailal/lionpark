7196fbfc9ea494c101354488e835006192a9df0a
Youness-Ailal <itsyounessaylal@gmail.com>
2023-10-31 22:46:04 +0100

    added animals page

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,10 @@
 import LazyLoad from "vanilla-lazyload";
-const lazyLoadInstance = new LazyLoad({});
-console.log(lazyLoadInstance);
+const lazyLoadInstance = new LazyLoad({
+  callback_loaded: el => {
+    if (
+      el.parentElement.previousElementSibling.classList.contains("lazy-loader")
+    ) {
+      el.parentElement.previousElementSibling.classList.add("hidden-loader");
+    }
+  },
+});
