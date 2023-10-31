d89d142ba1d49c5dc4286cc54fae2f79ea0b1f2f
Youness-Ailal <itsyounessaylal@gmail.com>
2023-11-01 00:24:35 +0100

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  output: "server",
+  adapter: netlify()
 });
--- src/script/main.js
+++ src/script/main.js
@@ -1,10 +1,15 @@
 import LazyLoad from "vanilla-lazyload";
 const lazyLoadInstance = new LazyLoad({
-  // callback_loaded: el => {
-  //   if (
-  //     el.parentElement.previousElementSibling.classList.contains("lazy-loader")
-  //   ) {
-  //     el.parentElement.previousElementSibling.classList.add("hidden-loader");
-  //   }
-  // },
+  callback_loaded: img => {
+    if (img?.previousElementSibling?.classList?.contains("lazy-loader")) {
+      img.previousElementSibling.classList.add("hidden-loader");
+    }
+    if (
+      img?.parentElement?.previousElementSibling?.classList?.contains(
+        "lazy-loader"
+      )
+    ) {
+      img.parentElement.previousElementSibling.classList.add("hidden-loader");
+    }
+  },
 });
