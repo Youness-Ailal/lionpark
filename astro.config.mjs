530ed4b0f2edfbd31699e0be4ccf72d232db606e
Youness-Ailal <itsyounessaylal@gmail.com>
2023-10-31 23:30:14 +0100

    added animals page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [netlify()],
+  // output: "server",
+  // integrations: [netlify()],
 });
